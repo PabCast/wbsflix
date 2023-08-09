0926e76a016a7bed30ef1a9ec132381c09056b02
PabCast <126684266+PabCast@users.noreply.github.com>
2023-08-09 10:07:32 +0200

    Add files via upload

--- Movie reccomender 1.2.py
+++ Movie reccomender 1.2.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import pandas as pd
 
+# Title for the App
+st.title('Personalized Movie Recommender')
+
+st.write("""
+Welcome to your personalized movie recommendation engine! 
+Simply provide your user ID and the number of recommendations you'd like,
+and we'll find the best movies tailored to your taste.
+""")
+
 # Loading DataFrames
 url_movies = 'https://drive.google.com/uc?id=1WB7QQGjulU_ODpfIAkey5ZO5dYDStkc_'
 movies_df = pd.read_csv(url_movies)
@@ -10,20 +19,8 @@ ratings_df = pd.read_csv(url_ratings)
 
 movies_ratings = movies_df.merge(ratings_df)
 
-def pop_movies(df_rate, df_movies, rate_tresh, range_of_days: float):
-    df_merge = (df_rate
-                .merge(df_movies, on='movieId')
-                .assign(time_count = round(abs(df_rate['timestamp'] - max(df_rate['timestamp'])) / 60 / 60 / 24))
-                .sort_values('time_count')
-                .filter(['userId', 'rating', 'title', 'time_count'])
-                .query('time_count<=@range_of_days')
-                )
-    df = df_merge.groupby('title').agg(rate_count=('rating', 'count'), rate_mean=('rating', 'mean')).query(
-        'rate_count >= @rate_tresh').sort_values('rate_mean', ascending=False)
-    return df.index.to_list()
-
 def get_fav_movie(df, userId):
-    user_top_movies = (movies_ratings[movies_ratings.userId == userId]
+    user_top_movies = (df[df.userId == userId]
          .sort_values('rating', ascending=False)
          .head(5)
          .reset_index()
@@ -32,11 +29,8 @@ def get_fav_movie(df, userId):
 
 def get_recommended_movies(df, top_movie, userId):
     list_of_movies = []
-    
-    # Drop columns with zero variance or only one unique value
     df = df.dropna(axis='columns', thresh=2)
     df = df.loc[:, (df != df.iloc[0]).any()]
-
     corr = df.corrwith(df[top_movie])
     lists = corr.sort_values(ascending=False).iloc[:50].index.to_list()
     for movie in lists:
@@ -54,15 +48,14 @@ def did_already_watch(df, movie, userId):
 
 def chat(df, userId, n_movies):
     movies_ratings_pivot = pd.pivot_table(df, values='rating', columns='title', index='userId')
-    st.write(f'''Hi {userId}! I am your personal recommender.
-    Here are {n_movies} popular movies based on your previous ratings:''')
+    st.write(f'''Hi {userId}! We've found {n_movies} amazing movies just for you:\n''')
     
     fav_movie = get_fav_movie(df, userId)['title'].iloc[0]
     list_of_movies = get_recommended_movies(movies_ratings_pivot, fav_movie, userId)[:n_movies]
     
     for movie in list_of_movies:
         genre = movies_df[movies_df['title'] == movie]['genres'].iloc[0]
-        st.write(f'''Title: {movie}, Genre: {genre}''')
+        st.write(f'''**Title:** {movie} | **Genre:** {genre}''')
 
 # Streamlit Interface
 user_id = st.number_input("Enter your user ID:", min_value=1, max_value=movies_ratings['userId'].max(), step=1)
