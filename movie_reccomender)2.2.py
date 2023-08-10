c7e43c97b000db07f3a57059d51d730930c7c741
Pablo Castro <126684266+PabCast@users.noreply.github.com>
2023-08-10 15:54:43 +0200

    Update movie_reccomender)2.2.py

--- movie_reccomender)2.2.py
+++ movie_reccomender)2.2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from surprise import Reader, Dataset, KNNBasic, accuracy
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.model_selection import train_test_split
 import random
