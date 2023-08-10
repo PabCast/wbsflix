aa8500ed9300d1e34a855e59f7f21b631bc2c40c
Pablo Castro <126684266+PabCast@users.noreply.github.com>
2023-08-10 17:25:18 +0200

    Update mrs_safeimg_test.py

--- mrs_safeimg_test.py
+++ mrs_safeimg_test.py
@@ -1,11 +1,7 @@
 import streamlit as st
 import pandas as pd
-from sklearn.decomposition import TruncatedSVD
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import Normalizer
-from sklearn.model_selection import train_test_split
 import requests
-import numpy as np
+from surprise import Reader, Dataset, SVD
 
 # Load data
 movies = pd.read_csv('data/movies.csv')
@@ -14,43 +10,44 @@ ratings = pd.read_csv('data/ratings.csv')
 # Combine the data into one dataset
 data_combined = pd.merge(movies, ratings)
 
-# Split into training and test set
-trainset, testset = train_test_split(data_combined, test_size=0.2)
-
-# Prepare data for training
-R_df = trainset.pivot(index='userId', columns='movieId', values='rating').fillna(0)
-R = R_df.values
-
-# Train SVD model
-svd = TruncatedSVD(n_components=200)
-normalizer = Normalizer(copy=False)
-lsa = make_pipeline(svd, normalizer)
-R_transformed = lsa.fit_transform(R)
-
-# Get popular movies
+# Calculate most popular movies by average rating
 popular_movies = data_combined.groupby(['movieId', 'title', 'genres'])['rating'].mean().reset_index()
 popular_movies = popular_movies.sort_values(by='rating', ascending=False)
 popular_movies = popular_movies.rename(columns={'rating': 'average_rating'})
 
+# Get top 10 popular movies
+top_10_popular_movies = popular_movies.head(10)
+
 # Add image URLs to popular movies DataFrame
 image_urls = []
-for movie_id in popular_movies.head(10)['movieId']:
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_API_KEY&append_to_response=images"
+for movie_id in top_10_popular_movies['movieId']:
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=da33e359269762e4cfacfd0eec2816a3&append_to_response=images"
     response = requests.get(url)
     images = response.json().get('images', {})
     image_url = images['posters'][0]['file_path'] if images.get('posters') else None
     image_urls.append(f"https://image.tmdb.org/t/p/w200{image_url}" if image_url else None)
 
-popular_movies['image_url'] = image_urls
+top_10_popular_movies['image_url'] = image_urls
 
 # Display popular movies
 st.title('Personalized Movie Recommender')
 st.subheader('Most Popular Movies')
-st.dataframe(popular_movies.head(10))
+st.dataframe(top_10_popular_movies[['title', 'genres', 'average_rating', 'image_url']])
 
 st.write("""
 Choose a user ID and find some great movie recommendations!
 """)
+
+# Read data using Surprise library
+reader = Reader(rating_scale=(1, 5))
+data = Dataset.load_from_df(data_combined[['userId', 'movieId', 'rating']], reader)
+
+# Split into training and test set
+trainset = data.build_full_trainset()
+
+# Train SVD algorithm
+algo = SVD()
+algo.fit(trainset)
 
 # Sidebar with user input
 user_id = st.sidebar.number_input("Enter your user ID:", min_value=1, max_value=ratings['userId'].max(), step=1)
@@ -58,26 +55,18 @@ n_movies = st.sidebar.number_input("Enter the number of movie recommendations yo
 
 # Button to generate recommendations
 if st.sidebar.button("Get Recommendations"):
-    # Get user recommendation
-    user_vector = R_transformed[user_id - 1, :]
-    predicted_ratings = np.dot(user_vector, svd.components_)
-    recommended_movie_ids = np.argsort(predicted_ratings)[::-1][:n_movies]
+    # Build anti-testset
+    testset = trainset.build_anti_testset()
+    testset = [x for x in testset if x[0] == user_id]
 
-    recommendations_df = pd.DataFrame(columns=['title', 'image_url'])
-    for movie_id in recommended_movie_ids:
-        movie_details = movies.loc[movies['movieId'] == movie_id].iloc[0]
-        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=da33e359269762e4cfacfd0eec2816a3&append_to_response=images"
-        response = requests.get(url)
-        images = response.json().get('images', {})
-        image_url = images['posters'][0]['file_path'] if images.get('posters') else None
-        recommendations_df = recommendations_df.append({
-            'title': movie_details['title'],
-            'image_url': f"https://image.tmdb.org/t/p/w200{image_url}" if image_url else None
-        }, ignore_index=True)
+    # Predict ratings
+    predictions = algo.test(testset)
 
-    st.dataframe(recommendations_df)
+    # Get top n recommendations
+    top_n_recs = sorted(predictions, key=lambda x: x.est, reverse=True)[:n_movies]
+    movie_recs = [movies.loc[movies['movieId'] == x.iid].iloc[0]['title'] for x in top_n_recs]
 
-    st.write(f"Here are the top {n_movies} movie recommendations for user {user_id}:")
-    for movie_id in recommended_movie_ids:
-        movie_details = movies.loc[movies['movieId'] == movie_id].iloc[0]
-        st.write(movie_details['title'])
+    # Display recommendations
+    rec_df = pd.DataFrame({'Movie Recommendations': movie_recs})
+    st.dataframe(rec_df)
+
